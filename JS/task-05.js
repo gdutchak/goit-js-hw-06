76e20699b47c8909977d2d13459d2718ee2adff6
gdutchak <galyna.dutchak99@gmail.com>
2022-07-13 20:55:48 +0300

    add final

--- JS/task-05.js
+++ JS/task-05.js
@@ -3,6 +3,11 @@ const nameEl = document.querySelector('#name-output')
 
 const inputUserName = (event) => {
     nameEl.textContent = event.target.value
+
+    if (!nameEl.textContent) {
+        nameEl.textContent = 'Anonymous'
+    }
+
 }
 inputEl.addEventListener('input', inputUserName)
 
